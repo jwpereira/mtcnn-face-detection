77942b7e509073d402aac87a56c40a3be1e88e5e
github-action-benchmark <github@users.noreply.github.com>
2020-12-29 23:39:02 +0000

    add Benchmark (googlecpp) benchmark result for a6eda320a64fd6fd6deae2526e22c88572faa21a

---
+++ dev/bench/data.js
@@ -0,0 +1,100 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1609285142154,
+  "repoUrl": "https://github.com/acampbel/mtcnn-face-detection",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "acampbel",
+            "username": "acampbel"
+          },
+          "committer": {
+            "name": "acampbel",
+            "username": "acampbel"
+          },
+          "id": "a6eda320a64fd6fd6deae2526e22c88572faa21a",
+          "message": "Update main.yml",
+          "timestamp": "2020-12-16T21:38:56Z",
+          "url": "https://github.com/acampbel/mtcnn-face-detection/pull/6/commits/a6eda320a64fd6fd6deae2526e22c88572faa21a"
+        },
+        "date": 1609285141534,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "tests.PerformanceTest/testDefaultDetect(imSize=small,imFaces=few)",
+            "value": 0.13357400000000003,
+            "unit": "s/iter",
+            "extra": "iterations: 8\ncpu: 0.13357400000000003 s\nthreads: 1"
+          },
+          {
+            "name": "tests.PerformanceTest/testDefaultDetect(imSize=small,imFaces=many)",
+            "value": 0.18933700000000003,
+            "unit": "s/iter",
+            "extra": "iterations: 4\ncpu: 0.18933700000000003 s\nthreads: 1"
+          },
+          {
+            "name": "tests.PerformanceTest/testDefaultDetect(imSize=med,imFaces=few)",
+            "value": 0.2936165,
+            "unit": "s/iter",
+            "extra": "iterations: 4\ncpu: 0.2936165 s\nthreads: 1"
+          },
+          {
+            "name": "tests.PerformanceTest/testDefaultDetect(imSize=med,imFaces=many)",
+            "value": 0.321308,
+            "unit": "s/iter",
+            "extra": "iterations: 4\ncpu: 0.321308 s\nthreads: 1"
+          },
+          {
+            "name": "tests.PerformanceTest/testDefaultDetect(imSize=large,imFaces=few)",
+            "value": 0.8061875,
+            "unit": "s/iter",
+            "extra": "iterations: 4\ncpu: 0.8061875 s\nthreads: 1"
+          },
+          {
+            "name": "tests.PerformanceTest/testDefaultDetect(imSize=large,imFaces=many)",
+            "value": 0.7865495,
+            "unit": "s/iter",
+            "extra": "iterations: 4\ncpu: 0.7865495 s\nthreads: 1"
+          },
+          {
+            "name": "tests.PerformanceTest/testLowLevelDetect(imSize=small,imFaces=few)",
+            "value": 0.1119425,
+            "unit": "s/iter",
+            "extra": "iterations: 4\ncpu: 0.1119425 s\nthreads: 1"
+          },
+          {
+            "name": "tests.PerformanceTest/testLowLevelDetect(imSize=small,imFaces=many)",
+            "value": 0.164503,
+            "unit": "s/iter",
+            "extra": "iterations: 4\ncpu: 0.164503 s\nthreads: 1"
+          },
+          {
+            "name": "tests.PerformanceTest/testLowLevelDetect(imSize=med,imFaces=few)",
+            "value": 0.2688035,
+            "unit": "s/iter",
+            "extra": "iterations: 4\ncpu: 0.2688035 s\nthreads: 1"
+          },
+          {
+            "name": "tests.PerformanceTest/testLowLevelDetect(imSize=med,imFaces=many)",
+            "value": 0.28181049999999996,
+            "unit": "s/iter",
+            "extra": "iterations: 4\ncpu: 0.28181049999999996 s\nthreads: 1"
+          },
+          {
+            "name": "tests.PerformanceTest/testLowLevelDetect(imSize=large,imFaces=few)",
+            "value": 0.793029,
+            "unit": "s/iter",
+            "extra": "iterations: 4\ncpu: 0.793029 s\nthreads: 1"
+          },
+          {
+            "name": "tests.PerformanceTest/testLowLevelDetect(imSize=large,imFaces=many)",
+            "value": 0.7761555,
+            "unit": "s/iter",
+            "extra": "iterations: 4\ncpu: 0.7761555 s\nthreads: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
