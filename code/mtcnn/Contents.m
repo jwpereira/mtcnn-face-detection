9d983d9a0ab1781a4ea0cd84e92309eda0028943
acampbel <47190204+acampbel@users.noreply.github.com>
2021-01-04 23:06:51 +0000

    Deploying to gh-pages from  @ 423ead63c944061144969e0e7b80617a3bbdcd11 🚀

---
+++ code/mtcnn/Contents.m
@@ -0,0 +1,8 @@
+% MTCNN
+% Version 1.2.2 (R2020a) 14-September-2020
+%
+% Files
+%   mtcnn.detectFaces   - Use a pretrained model to detect faces in an image.
+%   mtcnn.Detector      - Create a detector object to detect faces in an image.
+
+%  Copyright 2020 The MathWorks, Inc.
